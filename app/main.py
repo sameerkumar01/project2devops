74959210e4010b282ddf05aa354e428615a3f996
sameerkumar01 <139500172+sameerkumar01@users.noreply.github.com>
2025-07-28 11:53:56 +0530

    Update main.py

--- app/main.py
+++ app/main.py
@@ -9,7 +9,7 @@ API_KEY = os.environ.get("API_KEY", "default-key")
 
 @app.route("/")
 def hello():
-    return f"Hello! The API key starts with: {API_KEY[:4]}..."
+    return f"Hello from sameer!!! this is m api key: {API_KEY[:4]}..."
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=8080)
